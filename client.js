6950176c327fd30dc22bd3222fdfbcc41633d109
Dian Xiao <dian@Diandians-MBP.lan>
2023-09-23 20:44:22 -0700

    sanity client

--- client.js
+++ client.js
@@ -3,6 +3,7 @@ import sanityClient from '@sanity/client'
 
 export default sanityClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  apiVersion: '2023-09-21',
   dataset: 'production',
   useCdn: true
 })
