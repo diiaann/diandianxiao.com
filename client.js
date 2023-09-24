06137151c7d4c1bce771e5948681904cc1746654
Dian Xiao <dian@Diandians-MBP.lan>
2023-09-23 20:38:55 -0700

    update env

--- client.js
+++ client.js
@@ -2,7 +2,7 @@
 import sanityClient from '@sanity/client'
 
 export default sanityClient({
-  projectId: "jk66xbay",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: 'production',
   useCdn: true
 })
